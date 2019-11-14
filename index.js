b0a4f7aab22cea74b73e8885573d345354dcbaa8
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2019-11-14 10:57:13 -0800

    edited index.html, index.css, and index.js to create a working dog app

--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+let numberOfDogs = $('select').val()
+
+// Old function: this only works for one dog result.
+// function displayResults(responseJson) {
+//   console.log(responseJson);
+//   //replace the existing image with the new one
+//   $('.results-img').replaceWith(
+//     `<img src="${responseJson.message}" class="results-img">`
+//   )
+//   //display the results section
+//   $('.results').removeClass('hidden');
+// }
+
+// New Function: this works for many dogs results.  Also note that I got rid of the placeholder "results-img" img element and
+// removed the "hidden" class from the "results" section in index.html
+function displayAnotherResult(responseJson) {
+    console.log(responseJson);
+    //add the new image to the results
+    $('.results').append(`
+        <img src = ${responseJson.message} class="results-img" alt="dog">`
+    )
+}
+
+// Changes to getDogImage(): replaced displayResults(responseJson) with displayAnotherResult(responseJson).  This function
+// will run for the number of times requested in the select input.
+function getDogImage(i) {
+    console.log(`getDogImage ran and i is at ${i}` )
+    fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(responseJson => 
-      displayResults(responseJson))
+        displayAnotherResult(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
+function dogLoop(numberOfDogs) {
+    console.log(`number of dogs requested was ${numberOfDogs}`)
+    for (let i = 0; i < numberOfDogs; i++) {
+        getDogImage(i)
+    }
 }
 
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getDogImage();
-  });
+// function watchForm() {
+//   $('form').submit(event => {
+//     event.preventDefault();
+//     getDogImage();
+//   });
+// }
+
+function submitForm() {
+    $('.js-gimme-some-dogs').on('click', function() {
+        console.log('form was submitted')
+        event.preventDefault()
+        let numberOfDogs = $('select').val()
+        dogLoop(numberOfDogs)
+    })
 }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
-  watchForm();
+  console.log(numberOfDogs)
+  submitForm()
+  dogLoop()
+//   getDogImage()
+//   displayAnotherResult()
 });
